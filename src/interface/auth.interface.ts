67c4505e519fe7d51815b0b66fcfb8dd7c0a6f24
baongoc2211 <hoavienchon@gmai.com>
2025-03-08 11:03:41 +0700

    fix: Verify Email API

--- src/interface/auth.interface.ts
+++ src/interface/auth.interface.ts
@@ -13,3 +13,14 @@ export interface IInfo {
   phone: string;
   address: string;
 }
+export interface IVerify {
+  email: string;
+  OTP: string;
+}
+export interface IForgotPassword {
+  email: string;
+}
+export interface IReset {
+  OTP: string;
+  newPassword: string;
+}
